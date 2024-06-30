ac89d15db9bc08564b0c61403e974070d10a74a6
adityatiwari2408 <tiwariaditya445@gmail.com>
2024-06-30 17:14:13 +0530

    Commit

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getContact, createContact, UpdateContact, DeleteContact } = require('../controllers/Contactcontrollers');
 
 router.route("/").get(getContact).post(createContact);
-// router.route("/:id").put(UpdateContact).delete(DeleteContact);
+router.route("/:id").put(UpdateContact).delete(DeleteContact);
 
 
 module.exports = router;
